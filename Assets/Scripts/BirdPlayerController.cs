254b15ec5d11e65fb72702500d1592e1c3656dad
Dan Stevens <dan.stevens.iamai@gmail.com>
2020-05-04 20:02:49 +0100

    Day 1 of 100 Days of Code Challenge - creating a game in Unity
    Created a 2D project in Unity 2019.3, imported some assets from the [Flappy Bird Style Example Game](https://assetstore.unity.com/packages/templates/flappy-bird-style-example-game-80330).
    
    Created `BirdPlayerController` script and implemented vertical movement in accordance with the following spec:
    
        1. When the player pigeon is idle (i.e. neither up nor down key are pressed), the bird ‘glides’ down at a slow sleep.
        2. When the player presses the Up key, the bird jumps up a la Flappy Bird.
        3. When the player presses the Down key, the bird ‘dives’, falling quickly.

---
+++ Assets/Scripts/BirdPlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdPlayerController : MonoBehaviour
+{
+    [Header("Flight parameters")]
+    [SerializeField, Range(0, 100f)] float idleDrag = 10f;
+    [SerializeField, Range(0, 100f)] float diveDrag = 0f;
+    [SerializeField, Range(100, 1000f)] float upForce = 400f;
+
+    public string Greeting { get; set; } = "Hello";
+
+    private Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Called one per frame
+    private void Update()
+    {
+        // Prevent the bird from diving and flapping up in the same frame
+        var _ = DiveDown() || FlapUp();
+    }
+
+    private bool FlapUp()
+    {
+        if (Input.GetKeyUp(KeyCode.UpArrow)) {
+            rigidBody.velocity = Vector2.zero;
+            rigidBody.AddForce(new Vector2(0, upForce));
+            return true;
+        }
+        return false;
+    }
+
+    private bool DiveDown()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            rigidBody.velocity = Vector2.zero;
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            rigidBody.drag = diveDrag;
+            return true;
+        } else {
+            rigidBody.drag = idleDrag;
+            return false;
+        }
+    }
+}
