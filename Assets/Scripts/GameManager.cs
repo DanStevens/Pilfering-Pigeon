9e1f0a10bd9d74151768e7d602dfbdef9e08bb70
Dan Stevens <dan.stevens.iamai@gmail.com>
2020-05-11 21:49:53 +0100

    Day 5 of 100 Days of Code Challenge
    The ObjectScroller script now repositions the scrolling object once it has scrolled past the length of its collider. The two scrolling Ground objects work in tandem to provide an infinitely scrolling background.
    
    The current script only works with a left-scrolling background - TODO: get right-scrolling background working.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    
+    [Tooltip("The speed at which the ground scrolls. Negative values scroll to the left; positive to the right")]
+    [SerializeField] float groundScollSpeed = -2f;
+
     private ObjectScroller objectScroller;
 
     // Start is called before the first frame update
     void Start()
     {
         objectScroller = GetComponent<ObjectScroller>();
-        objectScroller.SetScrollSpeed(-2f);
+        objectScroller.SetScrollSpeed(groundScollSpeed);
     }
 
     // Update is called once per frame
--- Assets/Scripts/ObjectScroller.cs
+++ Assets/Scripts/ObjectScroller.cs
@@ -1,14 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 public class ObjectScroller : MonoBehaviour
 {
-    [SerializeField] Rigidbody2D[] objects;
+    [SerializeField] Rigidbody2D[] objects = new Rigidbody2D[] { };
 
-    // Update is called once per frame
+    [Tooltip("When true, objects are set back to their original position so that they can scroll indefinintely for a given area. " + 
+        "Useful to infininate scrolling backgrounds ")]
+    [SerializeField] bool carouselObjects = false;
+
+    void Update()
+    {
+        if (carouselObjects) {
+            foreach (var o in objects) {
+                if (o.TryGetComponent(out BoxCollider2D collider)) {
+                    if (o.transform.position.x < -collider.size.x)
+                        RepositionBackground(o, collider.size.x);
+                }
+            }
+        }
+    }
+
+    private static void RepositionBackground(Rigidbody2D o, float horizontalLength)
+    {
+        Vector2 groundOffset = new Vector2(horizontalLength * 2f, 0);
+        o.transform.position = (Vector2)o.transform.position + groundOffset;
+    }
+
     public void SetScrollSpeed(float speed)
     {
         foreach (Rigidbody2D rigidbody in objects)
