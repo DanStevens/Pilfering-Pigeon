98a251ef4108bcb9aa4d5e08d92d434daa3128f8
Dan Stevens <dan.stevens.iamai@gmail.com>
2020-05-13 15:31:18 +0100

    Day 7 of 100 Days of Code Challenge
    Added Collectable object that increments score.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,25 +2,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
     
     [Tooltip("The speed at which the ground scrolls. Negative values scroll to the left; positive to the right")]
-    [SerializeField] float groundScollSpeed = -2f;
+    [SerializeField] float globalScrollSpeed = -2f;
 
-    private ObjectScroller objectScroller;
+    [SerializeField] ObjectScroller[] objectScrollers;
+    [SerializeField] Text scoreText;
+
+    public int score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        objectScroller = GetComponent<ObjectScroller>();
-        objectScroller.SetScrollSpeed(groundScollSpeed);
+        foreach (var scroller in objectScrollers)
+            scroller.SetScrollSpeed(globalScrollSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void IncrementScore()
+    {
+        score++;
+        scoreText.text = $"Score: {score}";
     }
 }
--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Working WebGL play-testing builds availble for each day:
 [Day 1](https://danstevens.github.io/Pilfering-Pigeon/Day-001/)
 [Day 3](https://danstevens.github.io/Pilfering-Pigeon/Day-003/)
 [Day 4](https://danstevens.github.io/Pilfering-Pigeon/Day-004/)
+[Day 5](https://danstevens.github.io/Pilfering-Pigeon/Day-005/)
+
+# Attributions
+
+- [Flappy Bird Style](https://assetstore.unity.com/packages/templates/flappy-bird-style-example-game-80330) assets by [Unity Technologies](https://assetstore.unity.com/publishers/1)
+- [Free Pixel Food](https://assetstore.unity.com/packages/2d/environments/free-pixel-food-113523) Sprites by [Henry Software](https://assetstore.unity.com/publishers/9216)
